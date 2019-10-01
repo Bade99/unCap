18be6ccd2cb5abe5e6778526aaa27a1b4d181bc3
Bade99 <31745377+Bade99@users.noreply.github.com>
2019-10-01 02:44:40 -0300

    basic functions for subtitlefile object

--- SrtFix-Win32/SubtitleFile.h
+++ SrtFix-Win32/SubtitleFile.h
@@ -1,16 +1,31 @@
 #pragma once
+
+enum Encoding { UTF8, UTF16, ANSI };
+
 class SubtitleFile
 {
 private:
 	std::wstring file_fullpath;
 	std::wstring text;
-	enum Encoding encoding;
+	Encoding encoding;
 public:
-	enum Encoding { UTF8, UTF16, ANSI };
-	SubtitleFile(std::wstring file_fullpath);
+	SubtitleFile(std::wstring file_fullpath);//TODO(fran): we could throw an exception at creation time if file is invalid
 	~SubtitleFile();
-	Encoding GetEncoding();
-	void ApplyProperLineSeparation();
-	bool IsValid();//To check that the filepath that you gave it is a valid file
+
+	Encoding		GetEncoding();
+	void			SetEncoding(Encoding encoding);
+	void			SetEncodingFromFile();
+
+	std::wstring	GetFile_FullPath();
+	std::wstring	GetFile_Name();
+	std::wstring	GetFile_Extension();
+	std::wstring	GetFile_Directory();
+	void			SetFile(std::wstring file_fullpath);
+	
+	std::wstring	GetText();
+	void			SetText(std::wstring text);
+	
+	void			ApplyProperLineSeparationToText();//TODO(fran): private?
+	void			ReadFileToText();
 };
 
